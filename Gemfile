25c419aed38e8e2922212a695f265208848ab1fe
maximederoeux <mderoeux@yahoo.com>
2016-08-25 09:22:13 +0200

    Added devise and i18n

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem "font-awesome-rails"
+gem 'devise'
+gem 'rails-i18n'
 
 group :development, :test do
   gem 'byebug'
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,14 @@
 class Message < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true
+
+
+	def full_name
+		if first_name.present?
+			"#{first_name} #{name}"
+		else
+			name
+		end		
+	end
+	
 end
